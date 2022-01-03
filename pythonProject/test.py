0577adf9df83b1b22edf6dc2f30103078dcb569b
JOOSEOK LEE <pootter8@gmail.com>
2022-01-03 16:48:54 +0900

    first commit

---
+++ pythonProject/test.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 코딩 시작
+print(soup)
